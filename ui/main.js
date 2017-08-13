33f608ab209989b262ac335b52ca5727792bb353
Kamal1991 <kamalhotwani3@gmail.com>
2017-08-13 07:23:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,13 @@ var element=document.getElementById('main-text');
 element.innerHTML="NEW COURSE";
 //Move img
 var img=document.getElementById('img');
+var marginleft=0;
+function moveRight()
+{
+    marginleft=marginleft+10;
+    img.style.marginLeft=margingleft+'px';
+}
 img.onclick =function()
 {
-    img.style.marginLeft='100px';
+    var interval=setInterval(moveRight,100);
 };
