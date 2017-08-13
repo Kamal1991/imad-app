6e5ba0b622c0d497aeede1de2548dcebcf7fd58d
Kamal1991 <kamalhotwani3@gmail.com>
2017-08-13 19:47:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,15 @@
-console.log('Loaded!');
-var element=document.getElementById('main-text');
-element.innerHTML="NEW COURSE";
-//Move img
-var img=document.getElementById('img');
-var marginleft=0;
-function moveRight()
-{
-    marginleft=marginleft+50;
-    img.style.marginLeft=marginleft+'px';
-}
-img.onclick =function()
-{
-    var interval=setInterval(moveRight,1000);
+var button=document.getElementById('counter');
+button.onclick = function(){
+  var request=new XMLHttpRequest();
+  request.onreadystatechange=function(){
+      if(request.readyState===XMLHttpRequest.DONE){
+          if(request.status===200){
+              var counter=request.responseText;
+              var span=document.getElementById('count');
+              span.innerHTML=counter.toString();
+              }
+      }
+  };
+  request.open('GET','http://kamalhotwani3.imad.hasura-app.io/counter',true);
+  request.send(null);
 };
