bc50cb4c727dc06299e4b29d2bce84799366e39d
Kamal1991 <kamalhotwani3@gmail.com>
2017-08-13 20:19:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,5 +17,10 @@ var nameInput=document.getElementById('name');
 var name= nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
-    
+    var names=['name5','name4','name3','name2','name1'];
+    var list='';
+    for(var i=0;i<names.length;i++){
+        list+='<li>'+names[i]+'<li>';
+        }
+        var ui=document.getElementById('namelist');
 }
