32761be3d3ddc93fd619b51968eebdfc07c63085
Kamal1991 <kamalhotwani3@gmail.com>
2017-08-13 22:27:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -71,6 +71,12 @@ app.get('/counter', function (req, res) {
     counter=counter+1;
     res.send(counter.toString());
 });
+var names=[];
+app.get('/submit-name', function (req, res) {
+    var name=req.query.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
 
 app.get('/:contentname', function (req, res) {
     var contentname=req.params.contentname;
