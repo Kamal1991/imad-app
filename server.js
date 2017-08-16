a1da27bd64d8e9b05e0700baf1bf5bd36e4532cf
Kamal1991 <kamalhotwani3@gmail.com>
2017-08-16 13:01:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,15 +5,15 @@ app.use(morgan('combined'));
 
 var path = require('path');
 //var pool = require('pg').Pool;
-var config={
-    user:'kamalhotwani3',
-    database:'kamalhotwani3',
-    host:'db.imad.hasura-app.io',
-    port: '5432',
-    password : 'db-kamalhotwani3-62089'
-};
-var pool=new Pool(config);
-app.get('/test-db', function (req, res) {
+//var config={
+  //  user:'kamalhotwani3',
+ //   database:'kamalhotwani3',
+//    host:'db.imad.hasura-app.io',
+ //   port: '5432',
+ //   password : 'db-kamalhotwani3-62089'
+//};
+//var pool=new Pool(config);
+//app.get('/test-db', function (req, res) {
     pool.query('Select * from Test',function(err,result){
         if(err){
             res.status(500).send(err.toString());
