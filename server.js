e14b1899f49ea6bb93bff91f38e8bb9155e24f42
Kamal1991 <kamalhotwani3@gmail.com>
2017-08-17 09:46:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ app.get('/articles/:contentname', function (req, res) {
         if(err){
             res.status(500).send(err.toString());
         }else{
-            if(result.row.length===0){
+            if(result.row.Length===0){
                 res.status(404).send('Article Not Found');
             }else{
             var articleData=result.rows[0];
